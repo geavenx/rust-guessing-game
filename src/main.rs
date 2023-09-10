f099590ac4bbdaa7c5d5cf4c6d9f618c116ae156
geavenx <vitordonnangeloc@gmail.com>
2023-09-10 02:26:01 -0300

    final commit

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,39 @@
+extern crate rand;
+
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Please input your guess.");
 
-    println!("You guessed {}", guess);
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let parse_result = guess.trim().parse();
+
+        let guess: u32 = match parse_result {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+            Ordering::Greater => println!("Too big!"),
+        }
+    }
 }
